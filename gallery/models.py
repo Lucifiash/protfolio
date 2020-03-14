07ce705db03bc135e234d866e68e0c0add889f91
lucifiash <962862131@qq.com>
2020-03-14 15:14:04 +0800

    完善 Gallery APP

--- gallery/models.py
+++ gallery/models.py
@@ -2,4 +2,10 @@ from django.db import models
 
 # Create your models here.
 class Gallery(models.Model):
-    descripiton = models.CharField(max_length=50)
+    descripiton = models.CharField(default="在这写作品的描述",max_length=100)
+    #增加图片功能
+    image = models.ImageField(default="defalut.png" , upload_to ="images/")
+    title = models.CharField(default="作品标题",max_length=50)
+
+    def __str__(self):
+       return  self.title
