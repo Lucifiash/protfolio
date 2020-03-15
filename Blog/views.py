471fe162c9e3de6c5611a65cddf7089916d1a2aa
lucifiash <962862131@qq.com>
2020-03-15 10:39:34 +0800

    实现Blog到正文的跳转

--- Blog/views.py
+++ Blog/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 # Create your views here.
 def blog_page(request):
     blogs = Blog.objects
     return render(request,"Blog.html",{"blogs":blogs})
+def blog_text(request,blog_id):
+    blog = get_object_or_404(Blog, pk=blog_id)
+    return render(request,"Blog_text.html",{"blog":blog})
